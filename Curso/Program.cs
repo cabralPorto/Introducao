386020b83750a4dfc83f6c21f494e744f8dbcdaf
cabralPorto <ronaldocabralporto@gmail.com>
2022-01-04 20:28:33 -0300

    Inserindo dados em Massa

--- Curso/Program.cs
+++ Curso/Program.cs
@@ -11,8 +11,59 @@ namespace CursoEFCore
     {
         static void Main(string[] args)
         {
-           InserirDados();
+           //InserirDados();
+           InserirDadosEmMassa();
         }
+
+        private static void InserirDadosEmMassa()
+        {   
+            var produto = new Produto
+            {
+                 Descricao = "Produto Teste",
+                 CodigoBarras = "010203040506",
+                 Valor =90000m,
+                 TipoProduto = TipoProduto.MercadoriaParaRevenda,
+                 Ativo = true
+            };
+           
+           var cliente = new Cliente
+            {
+                Nome = "Ronaldo",
+                Telefone = "02122232425",
+                Cep = "11710040",
+                Estado = "SP",
+                Cidade = "Praia Grande"
+
+            }; 
+            var listaClientes = new []
+            {
+                new Cliente
+                {
+                Nome = "Ana Julia",
+                Telefone = "9897969594",
+                Cep = "11710040",
+                Estado = "SP",
+                Cidade = "Praia Grande"
+
+                },
+                new Cliente
+                {
+                Nome = "Beatriz",
+                Telefone = "02122232425",
+                Cep = "11710040",
+                Estado = "SP",
+                Cidade = "Praia Grande",
+                    
+                },
+
+            };
+            using var db = new Data.ApplicationContext();
+           // db.AddRange(produto, cliente);
+          // db.Clientes.AddRange(listaClientes);
+             
+            var registros = db.SaveChanges();
+             Console.WriteLine($"Total Registros (S): {registros}");
+        }         
         private static void InserirDados()
         {
             var produto = new Produto
